97096e8f665ac7cc41fcd3a08b56de900518fa25
wilfredo-marapacuto <wmarapacutoverastegu@byupathway.edu>
2026-05-15 14:06:29 -0600

    Connect PostgreSQL database

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+import 'dotenv/config';
+import { testConnection } from './src/models/db.js';
+
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
 const PORT = 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -38,6 +42,13 @@ app.get('/categories', async (req, res) => {
     res.render('categories', { title });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://127.0.0.1:${PORT}`);
+app.listen(PORT, async () => {
+    try {
+        await testConnection();
+
+        console.log(`Server running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
 });
